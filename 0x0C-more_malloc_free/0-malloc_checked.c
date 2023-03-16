cd483cc06c03fae400f7cbc9d08489005d40446c
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-16 16:35:54 -0700

    My first commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Main Entry
- * @b: input
- *
- * Return: 0
- */
+  * malloc_checked - Main Entry
+  * @b: input
+  *
+  * Return: 0
+  */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
