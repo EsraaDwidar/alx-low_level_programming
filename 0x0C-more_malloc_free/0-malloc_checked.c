89c1953dac447e87af305a531ef293d89fa5297d
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-17 11:47:37 -0700

    My first commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,6 +6,7 @@
 /**
  * malloc_checked - allocates memory to the pointer
  * @b: the input
+ *
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
@@ -13,7 +14,9 @@ void *malloc_checked(unsigned int b)
 	void *p;
 
 	p = malloc(b);
+
 	if (p == NULL)
 		exit(98);
+
 	return (p);
 }
