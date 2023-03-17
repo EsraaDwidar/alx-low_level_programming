7915bf1e05b7c74f54541dc60512148b5b18c5a1
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-17 11:33:25 -0700

    My first commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: input
- * Return: a pointer
+ * malloc_checked - allocates memory 
+ * @b: amount of bytes
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
