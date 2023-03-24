e9644bb6e71a9915c1e170a1fb84da28a49befeb
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-24 06:48:03 -0700

    My first commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to the function we need to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+        int i;
+
+	if (array !=NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(action) (array[i]);
+		}
+	}
+}
