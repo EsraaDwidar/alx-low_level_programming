068e1b65c72e1f02a0457c66a229e547b5bc745d
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-24 07:00:40 -0700

    My first commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,23 +2,21 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function
- * an carray
- * @array: array
+ * array_iterator - the function
  * @size: size of the array
  * @action: pointer to the function we need to use
- *
+ * @array: an array
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        unsigned int i;
+	unsigned int i;
 
-        if (array && action != NULL)
-        {
-                for (i = 0; i < size; i++)
-                {
-                        (action) (array[i]);
-                }
-        }
+	if (array && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(action) (array[i]);
+		}
+	}
 }
