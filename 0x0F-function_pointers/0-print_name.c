880ff18683c5782f68262d7b7c804edaa2cdeffa
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-24 06:37:23 -0700

    My first commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include "function_pointers.h"
+
 /**
  * print_name
+ *
  * @name: name
  * @f: function
- *
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
-		(f)(name);
+		(f) (name);
 }
