df2ef8fadce3d0294cc83cfb7aa34ac46f5a8649
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-24 16:27:21 -0700

    My first commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - the function
+ *
+ * @array: array to pass
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: the index of the first element for which the cmp function does not
+ * return 0. Otherwise, if no element matches or if size <= 0, return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp) (array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
