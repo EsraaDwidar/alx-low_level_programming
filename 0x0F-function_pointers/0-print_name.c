ca7398b610f276388c42cd75e703b1887fe2c8a0
EsraaDwidar <esraadwidar880@gmail.com>
2023-03-24 06:41:05 -0700

    My first commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name
+ * print_name - prints a name
+ * @name: string containing the name
+ * @f: pointer to function
  *
- * @name: name
- * @f: function
  */
 
 void print_name(char *name, void (*f)(char *))
